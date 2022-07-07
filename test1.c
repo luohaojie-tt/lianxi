c0198bddec8a44d1561c9ef911d42324e711ca2b
luohaojie <luohaojie@optel.com.cn>
2022-07-07 15:05:05 +0800

    first commit test1

---
+++ test1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+    int
+    main(void)
+{
+struct
+    {
+    int a;
+        char s[5];
+    float f;
+        
+    }
+    tt;
+char s[20];
+bzero(&tt, sizeof(tt)); //等价于memset(&tt,0,sizeof(tt));
+bzero(s, 20);           //等价于memset(s,0,20);
+printf("hello world");
+printf("Initial success.");
+getchar();
+return 0;
+
+}
