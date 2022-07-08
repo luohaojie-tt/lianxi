71b7f2d083c3c31112ccaa76ba977734cf4db9e1
luohaojie <luohaojie@optel.com.cn>
2022-07-08 11:22:11 +0800

    first commit test1

--- test1.c
+++ test1.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include <string.h>
-    int
-    main(void)
+int main(void)
 {
-struct
+    struct
     {
-    int a;
+        int a;
         char s[5];
-    float f;
-        
-    }
-    tt;
-char s[20];
-bzero(&tt, sizeof(tt)); //等价于memset(&tt,0,sizeof(tt));
-bzero(s, 20);           //等价于memset(s,0,20);
-printf("hello world");
-printf("Initial success.");
-getchar();
-return 0;
+        float f;
 
+    } tt;
+    char s[20];
+    bzero(&tt, sizeof(tt)); //等价于memset(&tt,0,sizeof(tt));
+    bzero(s, 20);           //等价于memset(s,0,20);
+    printf("hello world\n");
+    printf("Initial success.\n");
+    printf("hello luohaojie this is a test\n");
+    getchar();
+    printf("shuruwanbi");
+    return 0;
 }
